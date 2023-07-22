c78af5555e7cf2f278d434bafb18d7b1e58d53d8
Hugo Restrepo <analista2.tecnologia@laespanola.com.ec>
2023-07-22 15:01:12 -0500

    Creation of context
    Added required entity framework dependencies

---
+++ RepositoryLayer/PetStoreContext.cs
@@ -0,0 +1,20 @@
+﻿using DomainLayer.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepositoryLayer
+{
+    public class PetStoreContext : DbContext
+    {
+        public PetStoreContext(DbContextOptions<PetStoreContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Owner>? Owners { get; set; }
+        public DbSet<Pet>? Pets { get; set; }
+    }
+}
---
+++ UserControl/ContextFactory/RepositoryContextFactory.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using RepositoryLayer;
+
+namespace PresentationLayer.ContextFactory
+{
+    public class RepositoryContextFactory : IDesignTimeDbContextFactory<PetStoreContext>
+    {
+        public PetStoreContext CreateDbContext(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var builder = new DbContextOptionsBuilder<PetStoreContext>()
+                .UseSqlServer(configuration.GetConnectionString("sqlConnection"),
+                b => b.MigrationsAssembly("PresentationLayer"));
+
+            return new PetStoreContext(builder.Options);
+        }
+    }
+}
