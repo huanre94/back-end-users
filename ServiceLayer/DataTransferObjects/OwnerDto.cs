1341721567f424e57c50d7bdea0c85dde9a281e6
Hugo Restrepo <analista2.tecnologia@laespanola.com.ec>
2023-07-25 15:41:46 -0500

    Dto creation for models

---
+++ ServiceLayer/DataTransferObjects/OwnerDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceLayer.DataTransferObjects
+{
+    public record OwnerDto(long Id, string Name, string Telephone, string Email, bool Status);
+
+    public record OwnerCreateDto(string Name, string Telephone, string Email, bool Status);
+
+    public record OwnerUpdateDto(string Name, string Telephone, string Email, bool Status);
+}
---
+++ ServiceLayer/DataTransferObjects/PetDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceLayer.DataTransferObjects
+{
+    public record PetDto(long Id, string Name, DateTime? BirthDate, long OwnerId, bool Status);
+
+    public record PetCreateDto(string Name, DateTime? BirthDate);
+
+    public record PetUpdateDto(string Name, DateTime? BirthDate, long OwnerId);
+
+}
