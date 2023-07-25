b493a90ebe69ea7756468c9c0d2e75ee07160d23
Hugo Restrepo <huanre94@gmail.com>
2023-07-25 01:22:07 -0500

    Create interface for models

---
+++ RepositoryLayer/Contracts/IOwnerRepository.cs
@@ -0,0 +1,17 @@
+﻿using DomainLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepositoryLayer.Contracts
+{
+    public interface IOwnerRepository
+    {
+        IEnumerable<Owner> GetAllOwners(bool trackChanges = false);
+        Owner GetOwnerById(Guid ownerId, bool trackChanges = false);
+
+
+    }
+}
---
+++ RepositoryLayer/Contracts/IPetRepository.cs
@@ -0,0 +1,19 @@
+﻿using DomainLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepositoryLayer.Contracts
+{
+    public interface IPetRepository
+    {
+        IEnumerable<Pet> GetAllPets(bool trackChanges = false);
+        Pet GetPetById(bool trackChanges = false);
+        
+        IEnumerable<Pet> GetPetsByOwnerId(bool trackChanges = false);
+
+
+    }
+}
