6d9e3f78aec78d1a0a85923d2c528d0256eb120f
Hugo Restrepo <analista2.tecnologia@laespanola.com.ec>
2023-07-25 14:09:06 -0500

    Migrate for swap in AuditableEntity

--- UserControl/Migrations/20230725152842_ReplaceIdAndStatusInEntities.Designer.cs
+++ UserControl/Migrations/20230725152842_ReplaceIdAndStatusInEntities.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RepositoryLayer;
 
@@ -11,9 +12,11 @@ using RepositoryLayer;
 namespace PresentationLayer.Migrations
 {
     [DbContext(typeof(PetStoreContext))]
-    partial class PetStoreContextModelSnapshot : ModelSnapshot
+    [Migration("20230725152842_ReplaceIdAndStatusInEntities")]
+    partial class ReplaceIdAndStatusInEntities
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ UserControl/Migrations/20230725152842_ReplaceIdAndStatusInEntities.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PresentationLayer.Migrations
+{
+    /// <inheritdoc />
+    public partial class ReplaceIdAndStatusInEntities : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "PetId",
+                table: "Pets",
+                newName: "Id");
+
+            migrationBuilder.RenameColumn(
+                name: "OwnerId",
+                table: "Owners",
+                newName: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "Pets",
+                newName: "PetId");
+
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "Owners",
+                newName: "OwnerId");
+        }
+    }
+}
