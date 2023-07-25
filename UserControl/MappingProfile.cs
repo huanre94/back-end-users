4b0e4dc0bb3d833f45a35f8820ff939497d4c55b
Hugo Restrepo <analista2.tecnologia@laespanola.com.ec>
2023-07-25 15:42:44 -0500

    Create MappingProfile.cs

---
+++ UserControl/MappingProfile.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using DomainLayer.Models;
+using ServiceLayer.DataTransferObjects;
+
+namespace PresentationLayer
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Owner, OwnerDto>()
+                .ForCtorParam("Status", opt => opt.MapFrom(x => x.IsActive));
+
+
+            // CreateMap<OwnerDto, Owner>();
+            CreateMap<Pet, PetDto>()
+                    .ForCtorParam("Status", opt => opt.MapFrom(x => x.IsActive));
+
+            CreateMap<PetCreateDto, Pet>();
+
+            // CreateMap<PetDto, Pet>();
+        }
+    }
+}
